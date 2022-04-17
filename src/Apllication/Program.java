af8298f60a93bfa4afe264c09a2bc3c1c7224737
AntonioFogaça <antoniofogaca2012@gmail.com>
2022-04-16 22:51:49 -0300

    Insert implementation

--- src/Apllication/Program.java
+++ src/Apllication/Program.java
@@ -1,5 +1,6 @@
 package Apllication;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,6 +31,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n== teste 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Fogaça","antoniofogaca@gmail.com", new Date(),5000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inser! new id " + newSeller.getId());;
+      
 		
 		//System.out.println(obj);
 
